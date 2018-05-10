a5d005788e282a9b5abd6b42a9ba62dc5e781966
xingchen.lin <xingchen.lin@ele.me>
2018-05-10 10:19:04 +0800

    reinit

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,28 +1,9 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-  {
-    "id": "cordova-plugin-appversion.RareloopAppVersion",
-    "file": "plugins/cordova-plugin-appversion/www/app-version.js",
-    "pluginId": "cordova-plugin-appversion",
-    "clobbers": [
-      "AppVersion"
-    ]
-  },
-  {
-    "id": "cordova-plugin-app-update.AppUpdate",
-    "file": "plugins/cordova-plugin-app-update/www/AppUpdate.js",
-    "pluginId": "cordova-plugin-app-update",
-    "clobbers": [
-      "AppUpdate"
-    ]
-  }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-appversion": "1.0.0",
-  "cordova-plugin-app-update": "1.6.0"
+  "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
